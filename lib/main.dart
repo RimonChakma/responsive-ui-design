2590755f319ad1a9c485ab44e6ed157e7c1025a2
RimonChakma <rimon.chakma880@gmail.com>
2025-05-23 15:24:50 +0600

    create login screen

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,52 @@ class LoginScreens extends StatelessWidget {
     double buttonFont = isLargeScreen ? 18.0 : 16.0;
 
     return Scaffold(
-      body: Center(child: Text("responsive"),),
+      body: Center(child: Container(
+        constraints: BoxConstraints(maxWidth: 500),
+        padding: EdgeInsets.all(padding),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: screenSize.height * 0.05,),
+              Text("WellCome Back!",style: TextStyle(fontSize: isTitle),),
+              SizedBox(height: 8,),
+              Text("Login to continue",style: TextStyle(fontSize: isSubtitle,color: Colors.grey[600]),),
+              SizedBox(height: 32,),
+              Text("Email",style: TextStyle(fontSize: inputFont),),
+              const SizedBox(height: 8),
+              TextField(decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  hintText: "Enter your email"
+              ),),
+              const SizedBox(height: 20),
+              Text("Password",style: TextStyle(fontSize: inputFont),),
+              const SizedBox(height: 8),
+              TextField(decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15)
+                  ),
+                  hintText: "Enter your password"
+              ),),
+              const SizedBox(height: 32),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(onPressed: (){},style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15))
+                ), child: Text("Login",style: TextStyle(fontSize: buttonFont),)),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Don't have you account?",style: TextStyle(fontSize: inputFont),),
+                  TextButton(onPressed: (){}, child: Text("sign up",style: TextStyle(fontSize: inputFont),))
+                ],)
+            ],),
+        ),
+      ),),
     );
   }
 }
