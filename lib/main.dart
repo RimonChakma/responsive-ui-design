65a753008e7910a55fcc714d6a1313f7622223f4
RimonChakma <rimon.chakma880@gmail.com>
2025-05-24 23:34:07 +0600

    create responsive profile card

--- lib/main.dart
+++ lib/main.dart
@@ -22,10 +22,60 @@ class ProfileCardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+    final screenSize = MediaQuery.of(context).size;
+    final isLargeScreen = screenSize.width > 600;
+
+    double padding = isLargeScreen ? 30.0 : 16.0;
+    double isTitle = isLargeScreen ? 25.0 : 20.0;
+    double isSubtitle = isLargeScreen ? 24.0 : 18.0;
+    double description = isLargeScreen ? 16.0 : 14.0;
+
+    double buttonSize = isLargeScreen ? 300 : 200;
+    double buttonFont = isLargeScreen ? 18.0 : 16.0;
+
+    final textAlign = isLargeScreen ? TextAlign.left : TextAlign.left;
+    final avatarRadius = isLargeScreen ? 60.0 : 50.0;
 
     return Scaffold(
         body: Center(
-a
+          child: Padding(
+            padding: EdgeInsets.all(padding),
+            child: Container(
+              constraints: BoxConstraints(maxWidth: 600),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      radius: avatarRadius,
+                      backgroundImage: AssetImage("assets/man.png"),
+                    ),
+                    SizedBox(height: 30,),
+                    Text("RIMON CHAKMA",style: TextStyle(fontSize: isTitle),),
+                    Text("Software Engineer Flutter",style: TextStyle(fontSize: isSubtitle,color: Colors.lightBlue),),
+                    SizedBox(height: 20,),
+
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: isLargeScreen ? 60 :20),
+                      child: Text("A software developer designs,creates,and maintains software for various applications and systems.They are responsible for the entire software lifecycle",style: TextStyle(
+                        fontSize: description,),
+                        textAlign: textAlign,
+                      ),
+                    ),
+                    SizedBox(height: 20,),
+                    SizedBox(
+                      width: buttonSize,
+                      child: TextButton(onPressed: (){},style: TextButton.styleFrom(
+                          backgroundColor: Colors.blue,
+                          foregroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8))
+                      ), child: Text("Follow",style: TextStyle(fontSize: buttonFont),)),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
         )
     );
   }
