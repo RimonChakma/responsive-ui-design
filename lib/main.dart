e27e7094a2d211a61599d00e88165f47c55b5898
RimonChakma <rimon.chakma880@gmail.com>
2025-05-25 23:13:15 +0600

    create fb clone structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -16,67 +19,98 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class ProfileCardScreen extends StatelessWidget {
+class ProfileCardScreen extends StatefulWidget {
   const ProfileCardScreen({super.key});
 
   @override
+  State<ProfileCardScreen> createState() => _ProfileCardScreenState();
+}
+
+class _ProfileCardScreenState extends State<ProfileCardScreen> with TickerProviderStateMixin {
+  late TabController tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: 6, vsync: this)
+      ..addListener(() {
+        setState(() {});
+      });
+  }
+
+  @override
+  void dispose() {
+    tabController.dispose();
+    super.dispose();
+  }
+
+  Widget buildTabIcon(String path, int index, double size) {
+    return Image.asset(
+      path,
+      height: size,
+      width: size,
+      color: tabController.index == index ? Colors.blue : Colors.black,
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final List<String> tabIcon = [
+      "assets/home.png",
+      "assets/video.png",
+      "assets/people.png",
+      "assets/profile.png",
+      "assets/notification.png",
+      "assets/menu.png"
+    ];
 
     final screenSize = MediaQuery.of(context).size;
-    final isLargeScreen = screenSize.width > 600;
+    final screenWidth = screenSize.width;
+    final isLargeScreen = screenWidth > 600;
 
-    double padding = isLargeScreen ? 30.0 : 16.0;
-    double isTitle = isLargeScreen ? 25.0 : 20.0;
-    double isSubtitle = isLargeScreen ? 24.0 : 18.0;
-    double description = isLargeScreen ? 16.0 : 14.0;
+    double maxContainerWidth = isLargeScreen ? min(screenWidth * 0.85, 800) : double.infinity;
+    double iconSize = isLargeScreen ? 30 : 24;
+    double radius = isLargeScreen ? 28 : 22;
+    double text1 = isLargeScreen ? 30 : 22;
+    double storyWidth = min(screenWidth * 0.25, 120);
+    double storyTextSize = isLargeScreen ? 14 : 12;
 
-    double buttonSize = isLargeScreen ? 300 : 200;
-    double buttonFont = isLargeScreen ? 18.0 : 16.0;
-
-    final textAlign = isLargeScreen ? TextAlign.left : TextAlign.left;
-    final avatarRadius = isLargeScreen ? 60.0 : 50.0;
-
-    return Scaffold(
-        body: Center(
-          child: Padding(
-            padding: EdgeInsets.all(padding),
-            child: Container(
-              constraints: BoxConstraints(maxWidth: 600),
-              child: SingleChildScrollView(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CircleAvatar(
-                      radius: avatarRadius,
-                      backgroundImage: AssetImage("assets/man.png"),
-                    ),
-                    SizedBox(height: 30,),
-                    Text("RIMON CHAKMA",style: TextStyle(fontSize: isTitle),),
-                    Text("Software Engineer Flutter",style: TextStyle(fontSize: isSubtitle,color: Colors.lightBlue),),
-                    SizedBox(height: 20,),
-
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: isLargeScreen ? 60 :20),
-                      child: Text("A software developer designs,creates,and maintains software for various applications and systems.They are responsible for the entire software lifecycle",style: TextStyle(
-                        fontSize: description,),
-                        textAlign: textAlign,
+    return Center(
+      child: Container(
+        constraints: BoxConstraints(maxWidth: maxContainerWidth),
+        child: DefaultTabController(
+          length: 6,
+          child: Scaffold(
+            appBar: AppBar(
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "facebook",
+                    style: TextStyle(
+                        fontSize: text1, color: Colors.blue, fontWeight: FontWeight.w900),
+                  ),
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Image.asset("assets/search.png", height: iconSize, width: iconSize),
                       ),
-                    ),
-                    SizedBox(height: 20,),
-                    SizedBox(
-                      width: buttonSize,
-                      child: TextButton(onPressed: (){},style: TextButton.styleFrom(
-                          backgroundColor: Colors.blue,
-                          foregroundColor: Colors.white,
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8))
-                      ), child: Text("Follow",style: TextStyle(fontSize: buttonFont),)),
-                    )
-                  ],
-                ),
+                      SizedBox(width: 5),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Image.asset("assets/messenger.png", height: iconSize, width: iconSize),
+                      ),
+                    ],
+                  )
+                ],
               ),
+
             ),
+
           ),
-        )
+        ),
+      ),
     );
   }
 }
